41fe0f1488cef39dcdb0fdbc4b7ede54ba0ed370
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-11-04 22:17:36 +0530

    Add files via upload

---
+++ Hello_world.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package P1_hello;
+
+/**
+ *
+ * @author Priyanka N R
+ */
+public class Hello_world {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+}
