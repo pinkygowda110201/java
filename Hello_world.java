dbc8b1490cc00515141fe22309c0d9b03c721f04
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-11-04 22:18:26 +0530

    Update Hello_world.java

--- Hello_world.java
+++ Hello_world.java
@@ -1,17 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package P1_hello;
-
-/**
- *
- * @author Priyanka N R
- */
 public class Hello_world {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
-    
 }
