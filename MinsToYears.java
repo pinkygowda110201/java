f79c968bd1c72d0a400cf92527b7526ec80c21bc
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-11-15 14:13:26 +0530

    Update MinsToYears.java

--- MinsToYears.java
+++ MinsToYears.java
@@ -4,11 +4,11 @@ public class MinsToYears {
     public static void main (String[] args){
         
     System.out.println("Input the number of minutes:");
-    Scanner sc = new Scanner(System.in);
-    long mins = sc.nextInt();
-    long days = mins / (24*60);
-    long yrs = mins / (365*24*60);
-    long remainingDays = (mins-(yrs*365*24*60))/(24*60);
+    Scanner sc = new Scanner(System.in); //Taking input from user
+    long mins = sc.nextInt(); 
+    long days = mins / (24*60); //Converting minutes to days
+    long yrs = days / 365; //Converting to years
+    long remainingDays = (mins-(yrs*365*24*60))/(24*60); //Calculating remaining days
     System.out.println(mins+" minutes is approximately "+yrs+" years and "+remainingDays+" days.");
     }
 }
